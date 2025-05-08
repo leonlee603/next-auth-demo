d6f9a840cef9e3bb8ed74a2bf5ea34d648eebcd7
Leon <leepingho603@gmail.com>
2025-05-08 15:09:36 +0800

    Add my-account page and change-password page

---
+++ app/(logged-in)/change-password/page.tsx
@@ -0,0 +1,14 @@
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+
+export default function ChangePasswordPage() {
+  return (
+    <Card className="w-[350px]">
+      <CardHeader>
+        <CardTitle>Change Password</CardTitle>
+      </CardHeader>
+      <CardContent>
+        {/* <ChangePasswordForm /> */}
+      </CardContent>
+    </Card>
+  );
+}
