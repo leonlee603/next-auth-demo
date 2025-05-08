43d3f25a2d367d7126d296683656362e8be3c8c0
Leon <leepingho603@gmail.com>
2025-05-08 15:08:00 +0800

    Add user id to jwt

--- auth.ts
+++ auth.ts
@@ -7,6 +7,18 @@ import { db } from "./db/drizzle";
 import { users } from "./db/usersSchema";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  callbacks: {
+    jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+    session({ session, token }) {
+      session.user.id = token.id as string;
+      return session;
+    },
+  },
   providers: [
     Credentials({
       credentials: {
