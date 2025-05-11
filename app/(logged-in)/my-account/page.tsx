87703a2d999a225bacf74cee8a79128e7ff590e7
Leon Lee <leepingho603@gmail.com>
2025-05-11 22:54:13 +0800

    Add two factor auth form

--- app/(logged-in)/my-account/page.tsx
+++ app/(logged-in)/my-account/page.tsx
@@ -1,10 +1,21 @@
 import { auth } from "@/auth";
+import { eq } from "drizzle-orm";
 
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Label } from "@/components/ui/label";
+import TwoFactorAuthForm from "./two-factor-auth-form";
+import { db } from "@/db/drizzle";
+import { users } from "@/db/usersSchema";
 
 export default async function MyAccountPage() {
   const session = await auth();
+
+  const [user] = await db
+    .select({
+      twoFactorActivated: users.twoFactorActivated,
+    })
+    .from(users)
+    .where(eq(users.id, parseInt(session!.user!.id!)));
 
   return (
     <Card className="w-[350px]">
@@ -14,6 +25,9 @@ export default async function MyAccountPage() {
       <CardContent>
         <Label>Email Address</Label>
         <div className="text-muted-foreground">{session?.user?.email}</div>
+        <TwoFactorAuthForm
+          twoFactorActivated={user.twoFactorActivated ?? false}
+        />
       </CardContent>
     </Card>
   );
---
+++ app/(logged-in)/my-account/two-factor-auth-form/index.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+
+type Props = {
+  twoFactorActivated: boolean;
+};
+
+export default function TwoFactorAuthForm({ twoFactorActivated }: Props) {
+  const [isActivated, setIsActivated] = useState(twoFactorActivated);
+  const [step, setStep] = useState(1);
+
+  const handleEnableClick = async () => {
+    setStep(2);
+  };
+
+  const handleOTPSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+  };
+
+  return (
+    <div>
+      {!isActivated && (
+        <div>
+          {step === 1 && (
+            <Button onClick={handleEnableClick}>
+              Enable Two-Factor Authentication
+            </Button>
+          )}
+          {step === 2 && (
+            <div>
+              <p className="text-xs text-muted-foreground py-2">
+                Scan the QR code below in the Google Authenticator app to
+                activate Two-Factor Authentication.
+              </p>
+              {/* <QRCode value={code} /> */}
+              <Button onClick={() => setStep(3)} className="w-full my-2">
+                I have scanned the QR code
+              </Button>
+              <Button
+                onClick={() => setStep(1)}
+                variant="outline"
+                className="w-full my-2"
+              >
+                Cancel
+              </Button>
+            </div>
+          )}
+          {step === 3 && (
+            <form onSubmit={handleOTPSubmit} className="flex flex-col gap-2">
+              <p className="text-xs text-muted-foreground">
+                Please enter the one-time passcode from the Google Authenticator
+                app.
+              </p>
+            </form>
+          )}
+        </div>
+      )}
+    </div>
+  );
+}
